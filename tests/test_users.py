a321141414fe4d6d18c0adcd606316b6ff14f21f
mariamts12 <matsut21@freeuni.edu.ge>
2024-02-02 01:57:12 +0400

    user tests added

---
+++ tests/test_users.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass, field
+from typing import Generator
+from unittest.mock import ANY
+
+import pytest
+from faker import Faker
+from fastapi.testclient import TestClient
+
+from runner.constants import TEST_DATABASE_NAME
+from runner.setup import init_app
+
+
+@dataclass
+class Fake:
+    faker: Faker = field(default_factory=Faker)
+
+    def user(self) -> dict[str, str]:
+        return {"email": self.faker.catch_phrase()}
+
+
+@pytest.fixture
+def client() -> Generator[TestClient, None, None]:
+    app = init_app(TEST_DATABASE_NAME)
+    with TestClient(app) as client:
+        yield client
+
+
+def test_should_create(client: TestClient) -> None:
+    user = Fake().user()
+
+    response = client.post("/users", json=user)
+
+    assert response.status_code == 201
+    assert response.json() == {"id": ANY}
+    client.app.state.users.clear()  # type: ignore
+
+
+def test_should_not_create(client: TestClient) -> None:
+    user = Fake().user()
+
+    response = client.post("/users", json=user)
+    user_email = user["email"]
+    assert response.status_code == 201
+    assert response.json() == {"id": ANY}
+
+    response = client.post("/users", json=user)
+    assert response.status_code == 409
+    assert response.json() == {
+        "message": f"User with email<{user_email}> already exists."
+    }
+    client.app.state.users.clear()  # type: ignore
