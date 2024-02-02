903bea09cbe5b30e48e37184218118d1e2585599
Ana Sepiashvili <asepi21@freeuni.edu.ge>
2024-02-02 14:54:12 +0400

    updated args of exception

--- infra/repositories/wallet_repository.py
+++ infra/repositories/wallet_repository.py
@@ -1,4 +1,3 @@
-from typing import Protocol
 from uuid import UUID
 
 from core.errors import DoesNotExistError, ThreeWalletsError
@@ -6,32 +5,11 @@ from core.wallet import Wallet
 from infra.repositories.database import DatabaseHandler
 
 
-class WalletRepository(Protocol):
-    def __init__(self, db: DatabaseHandler, table: str, vals: str) -> None:
-        pass
-
-    def create(self) -> None:
-        pass
-
-    def add(self, wallet: Wallet) -> None:
-        pass
-
-    def read_with_wallet_id(self, wallet_id: UUID) -> Wallet:
-        pass
-
-    def has_same_owner(self, wallet_id1: UUID, wallet_id2: UUID) -> bool:
-        pass
-
-    def make_transaction(self, transaction: list) -> None:
-        pass
-
-
-class SqlWalletRepository(WalletRepository):
-    def __init__(self, db: DatabaseHandler, table: str, vals: str) -> None:
-        self.database = db
-        self.table_name = table
-        self.columns = vals
-        db.create_table(self.table_name, self.columns)
+class SqlWalletRepository:
+    def __init__(self, database: DatabaseHandler, table_name: str, columns: str):
+        self.database = database
+        self.table_name = table_name
+        self.columns = columns
 
     def create(self) -> None:
         self.database.create_table(self.table_name, self.columns)
@@ -41,7 +19,7 @@ class SqlWalletRepository(WalletRepository):
             cursor = connection.cursor()
             cursor.execute(
                 f"SELECT * FROM {self.table_name} "
-                f"WHERE owner_id = '{wallet.get_owner_id()}'"
+                f"WHERE owner_id = {wallet.get_owner_id()}"
             )
             elems = cursor.fetchall()
             if len(elems) == 3:
@@ -50,21 +28,16 @@ class SqlWalletRepository(WalletRepository):
                 query = f"INSERT INTO {self.table_name} VALUES (?, ?, ?)"
                 cursor.execute(
                     query,
-                    (
-                        str(wallet.get_id()),
-                        str(wallet.get_owner_id()),
-                        wallet.get_balance(),
-                    ),
+                    (wallet.get_id(), wallet.owner_id, wallet.balance),
                 )
                 connection.commit()
 
     def read_with_wallet_id(self, wallet_id: UUID) -> Wallet:
         with self.database.connect() as connection:
             cursor = connection.cursor()
-            print("WOLIT ID")
-            print(wallet_id)
             cursor.execute(
-                f"SELECT * FROM {self.table_name}" f" WHERE wallet_id = '{wallet_id}'"
+                f"SELECT * FROM {self.table_name}"
+                f" WHERE wallet_id = {wallet_id}"
             )
             values = cursor.fetchone()
             if values is None:
@@ -72,43 +45,19 @@ class SqlWalletRepository(WalletRepository):
             else:
                 return Wallet(UUID(values[1]), values[2], UUID(values[0]))
 
-    def has_same_owner(self, wallet_id1: UUID, wallet_id2: UUID) -> bool:
-        with self.database.connect() as connection:
-            cursor = connection.cursor()
-            cursor.execute(
-                f"SELECT owner_id FROM {self.table_name}"
-                f" WHERE wallet_id = '{wallet_id1}'"
-            )
-            value1 = cursor.fetchone()
-            cursor.execute(
-                f"SELECT owner_id FROM {self.table_name}"
-                f" WHERE wallet_id = '{wallet_id2}'"
-            )
-            value2 = cursor.fetchone()
-            if value1 is None or value2 is None:
-                raise DoesNotExistError()
-            elif value1[0] == value2[0]:
-                return True
-            else:
-                return False
-
     def make_transaction(self, transaction: list) -> None:
         with self.database.connect() as connection:
             cursor = connection.cursor()
-            cursor.execute(
-                f"SELECT * FROM {self.table_name} " f" WHERE wallet_id = ?", ()
-            )
+            cursor.execute(f"SELECT * FROM {self.table_name} WHERE id = ?", ())
             values1 = cursor.fetchone()
-            cursor.execute(
-                f"SELECT * FROM {self.table_name} " f" WHERE wallet_id = ?", ()
-            )
+            cursor.execute(f"SELECT * FROM {self.table_name} WHERE id = ?", ())
             values2 = cursor.fetchone()
             if values1 is None or values2 is None:
                 raise DoesNotExistError()
             else:
                 cursor.execute(
                     f"UPDATE {self.table_name} SET balance = {transaction}"
-                    f" WHERE id = '{values1}'"
+                    f"WHERE id = '{values1}'"
                 )
                 cursor.execute(
                     f"UPDATE {self.table_name} SET balance = {transaction}"
